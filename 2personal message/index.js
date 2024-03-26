4071031da920cf061ae2dcbc103e67e50aeac680
Aqsawaqar55 <156216965+Aqsawaqar55@users.noreply.github.com>
2024-03-27 01:54:40 +0500

    first commit

---
+++ 2personal message/index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let personName = "AQSA";
+console.log(`hello${personName},would you like to learn some python today?`);
---
+++ 2personal message/index.ts
@@ -0,0 +1,2 @@
+let personName:String="AQSA";
+console.log(`hello${personName},would you like to learn some python today?`);
